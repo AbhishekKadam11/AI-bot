d1a673b706fde139f5bdb10e4d265e487cc91280
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-09-22 18:40:00 +0530

    -Working on dialogflow integration.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleProjectID: 'newtech-4f4cc',
+    dialogFlowSessionID: 'newtech-session',
+    dialogFlowSessionLanguageCode: 'en-US'
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('test')
-});
+app.use(bodyParser.json());
+
+require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
