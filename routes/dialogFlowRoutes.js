91d8618a11265617132aec634f3a35cc83d211a2
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-10-02 19:14:34 +0530

    -Working on production environment.

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -7,7 +7,12 @@ module.exports = app => {
     });
 
     app.post('/api/test', async (req, res) => {
-        let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
-        res.send(responses[0].queryResult);
+        try{
+            let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+            res.send(responses[0].queryResult);
+        }catch (e) {
+            console.log(e)
+        }
+
     });
 };
