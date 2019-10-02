df8a0eef3fc9149289f68cbd755a1eeb021470d1
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-10-02 18:59:49 +0530

    -Working on production environment.

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -9,40 +9,39 @@ const credentails = {
 };
 const sessionClient = new dialogflow.SessionsClient({projectID, credentails});
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
-const app = dialogflow({
-    debug: true
-});
+console.log("test "+ JSON.stringify(sessionClient));
 module.exports = {
     textQuery: async function(text, parameters = {}) {
         let self = module.exports;
         // The text query request.
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    // The query to send to the dialogflow agent
-                    text: text,
-                    // The language used by the client (en-US)
-                    languageCode: config.dialogFlowSessionLanguageCode,
+        try{
+            const request = {
+                session: sessionPath,
+                queryInput: {
+                    text: {
+                        // The query to send to the dialogflow agent
+                        text: text,
+                        // The language used by the client (en-US)
+                        languageCode: config.dialogFlowSessionLanguageCode,
+                    },
                 },
-            },
-            queryParams: {
-                payload: {
-                    data: parameters
+                queryParams: {
+                    payload: {
+                        data: parameters
+                    }
                 }
-            }
-        };
-        app.catch((conv, error) => {
-            console.error(error);
-            conv.ask('I encountered a glitch. Can you say that again?');
-        });
-        // Send request and log result
-        let responses = await sessionClient
-            .detectIntent(request)
-            .catch(errorCallback);
-        responses = await self.handleAction(responses);
-       // res.send(responses[0].queryResult);
-        return responses;
+            };
+            // Send request and log result
+            let responses = await sessionClient
+                .detectIntent(request)
+            ;
+            responses = await self.handleAction(responses);
+            // res.send(responses[0].queryResult);
+            return responses;
+        } catch (e) {
+            console.log(JSON.stringify(e));
+        }
+
     },
 
     handleAction: function(responses) {
